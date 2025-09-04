ab042474fd892287f3d38ba8d59ee450278658ad
Mrhell01 <panchalkaran967@gmail.com>
2025-09-04 20:38:07 +0530

    Added fallback pages

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -11,6 +11,11 @@
 import { Route as rootRouteImport } from './routes/__root'
 import { Route as AboutRouteImport } from './routes/about'
 import { Route as IndexRouteImport } from './routes/index'
+import { Route as errors503RouteImport } from './routes/(errors)/503'
+import { Route as errors500RouteImport } from './routes/(errors)/500'
+import { Route as errors404RouteImport } from './routes/(errors)/404'
+import { Route as errors403RouteImport } from './routes/(errors)/403'
+import { Route as errors401RouteImport } from './routes/(errors)/401'
 import { Route as dashboardOrganisationDashboardRouteImport } from './routes/(dashboard)/organisationDashboard'
 import { Route as dashboardGeneralUserDashBoardRouteImport } from './routes/(dashboard)/generalUserDashBoard'
 import { Route as dashboardAdminDashboardRouteImport } from './routes/(dashboard)/adminDashboard'
@@ -26,6 +31,31 @@ const AboutRoute = AboutRouteImport.update({
 const IndexRoute = IndexRouteImport.update({
   id: '/',
   path: '/',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const errors503Route = errors503RouteImport.update({
+  id: '/(errors)/503',
+  path: '/503',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const errors500Route = errors500RouteImport.update({
+  id: '/(errors)/500',
+  path: '/500',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const errors404Route = errors404RouteImport.update({
+  id: '/(errors)/404',
+  path: '/404',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const errors403Route = errors403RouteImport.update({
+  id: '/(errors)/403',
+  path: '/403',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const errors401Route = errors401RouteImport.update({
+  id: '/(errors)/401',
+  path: '/401',
   getParentRoute: () => rootRouteImport,
 } as any)
 const dashboardOrganisationDashboardRoute =
@@ -70,6 +100,11 @@ export interface FileRoutesByFullPath {
   '/adminDashboard': typeof dashboardAdminDashboardRoute
   '/generalUserDashBoard': typeof dashboardGeneralUserDashBoardRoute
   '/organisationDashboard': typeof dashboardOrganisationDashboardRoute
+  '/401': typeof errors401Route
+  '/403': typeof errors403Route
+  '/404': typeof errors404Route
+  '/500': typeof errors500Route
+  '/503': typeof errors503Route
 }
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
@@ -80,6 +115,11 @@ export interface FileRoutesByTo {
   '/adminDashboard': typeof dashboardAdminDashboardRoute
   '/generalUserDashBoard': typeof dashboardGeneralUserDashBoardRoute
   '/organisationDashboard': typeof dashboardOrganisationDashboardRoute
+  '/401': typeof errors401Route
+  '/403': typeof errors403Route
+  '/404': typeof errors404Route
+  '/500': typeof errors500Route
+  '/503': typeof errors503Route
 }
 export interface FileRoutesById {
   __root__: typeof rootRouteImport
@@ -91,6 +131,11 @@ export interface FileRoutesById {
   '/(dashboard)/adminDashboard': typeof dashboardAdminDashboardRoute
   '/(dashboard)/generalUserDashBoard': typeof dashboardGeneralUserDashBoardRoute
   '/(dashboard)/organisationDashboard': typeof dashboardOrganisationDashboardRoute
+  '/(errors)/401': typeof errors401Route
+  '/(errors)/403': typeof errors403Route
+  '/(errors)/404': typeof errors404Route
+  '/(errors)/500': typeof errors500Route
+  '/(errors)/503': typeof errors503Route
 }
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
@@ -103,6 +148,11 @@ export interface FileRouteTypes {
     | '/adminDashboard'
     | '/generalUserDashBoard'
     | '/organisationDashboard'
+    | '/401'
+    | '/403'
+    | '/404'
+    | '/500'
+    | '/503'
   fileRoutesByTo: FileRoutesByTo
   to:
     | '/'
@@ -113,6 +163,11 @@ export interface FileRouteTypes {
     | '/adminDashboard'
     | '/generalUserDashBoard'
     | '/organisationDashboard'
+    | '/401'
+    | '/403'
+    | '/404'
+    | '/500'
+    | '/503'
   id:
     | '__root__'
     | '/'
@@ -123,6 +178,11 @@ export interface FileRouteTypes {
     | '/(dashboard)/adminDashboard'
     | '/(dashboard)/generalUserDashBoard'
     | '/(dashboard)/organisationDashboard'
+    | '/(errors)/401'
+    | '/(errors)/403'
+    | '/(errors)/404'
+    | '/(errors)/500'
+    | '/(errors)/503'
   fileRoutesById: FileRoutesById
 }
 export interface RootRouteChildren {
@@ -134,6 +194,11 @@ export interface RootRouteChildren {
   dashboardAdminDashboardRoute: typeof dashboardAdminDashboardRoute
   dashboardGeneralUserDashBoardRoute: typeof dashboardGeneralUserDashBoardRoute
   dashboardOrganisationDashboardRoute: typeof dashboardOrganisationDashboardRoute
+  errors401Route: typeof errors401Route
+  errors403Route: typeof errors403Route
+  errors404Route: typeof errors404Route
+  errors500Route: typeof errors500Route
+  errors503Route: typeof errors503Route
 }
 
 declare module '@tanstack/react-router' {
@@ -150,6 +215,41 @@ declare module '@tanstack/react-router' {
       path: '/'
       fullPath: '/'
       preLoaderRoute: typeof IndexRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/(errors)/503': {
+      id: '/(errors)/503'
+      path: '/503'
+      fullPath: '/503'
+      preLoaderRoute: typeof errors503RouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/(errors)/500': {
+      id: '/(errors)/500'
+      path: '/500'
+      fullPath: '/500'
+      preLoaderRoute: typeof errors500RouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/(errors)/404': {
+      id: '/(errors)/404'
+      path: '/404'
+      fullPath: '/404'
+      preLoaderRoute: typeof errors404RouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/(errors)/403': {
+      id: '/(errors)/403'
+      path: '/403'
+      fullPath: '/403'
+      preLoaderRoute: typeof errors403RouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/(errors)/401': {
+      id: '/(errors)/401'
+      path: '/401'
+      fullPath: '/401'
+      preLoaderRoute: typeof errors401RouteImport
       parentRoute: typeof rootRouteImport
     }
     '/(dashboard)/organisationDashboard': {
@@ -206,6 +306,11 @@ const rootRouteChildren: RootRouteChildren = {
   dashboardAdminDashboardRoute: dashboardAdminDashboardRoute,
   dashboardGeneralUserDashBoardRoute: dashboardGeneralUserDashBoardRoute,
   dashboardOrganisationDashboardRoute: dashboardOrganisationDashboardRoute,
+  errors401Route: errors401Route,
+  errors403Route: errors403Route,
+  errors404Route: errors404Route,
+  errors500Route: errors500Route,
+  errors503Route: errors503Route,
 }
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,7 @@
 import { createRootRoute,Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import GeneralError from '@/features/errors/general-error'
+import NotFoundError from '@/features/errors/not-found-error'
 
 export const Route = createRootRoute({
   component: () => (
@@ -8,4 +10,6 @@ export const Route = createRootRoute({
       <TanStackRouterDevtools />
     </>
   ),
+  notFoundComponent: NotFoundError,
+  errorComponent: GeneralError,
 })
